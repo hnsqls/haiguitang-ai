cc04bbe2be141dafbb5388a589e963ac60c021fd
hnsqls <2661108038@qq.com>
2025-03-17 09:56:46 +0800

    题目介绍，后端项目初始化

---
+++ haiguitang-ai/haiguitang-ai-backend/src/main/java/com/ls/ai/AIconfig/Aiconfig.java
@@ -0,0 +1,10 @@
+package com.ls.ai.AIconfig;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AI 配置类
+ */
+@Configuration
+public class Aiconfig {
+}
---
+++ haiguitang-ai/haiguitang-ai-backend/src/main/java/com/ls/ai/manager/Aimanager.java
@@ -0,0 +1,10 @@
+package com.ls.ai.manager;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Ai 工具类
+ */
+@Component
+public class Aimanager {
+}
